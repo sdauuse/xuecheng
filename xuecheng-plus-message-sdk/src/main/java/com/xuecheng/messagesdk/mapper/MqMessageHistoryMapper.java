44f3a962ae0f26d33d668f1b370b0a4121700659
sdauuse <751648872@qq.com>
2023-06-13 16:38:31 +0800

    新增课程发布相关sdk，修复多模块找不到包的bug，完善了部分课程发布功能

---
+++ xuecheng-plus-message-sdk/src/main/java/com/xuecheng/messagesdk/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.messagesdk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.messagesdk.model.po.MqMessageHistory;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ */
+@Mapper
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
